b34a85d1977c929822446f6c876e0b0fa92db386
gayanataraj9742-blip <gayanataraj9742@gmail.com>
2026-06-10 22:52:01 +0530

    Add filled shapes to 2D Graphics Editor

--- canvas1.c
+++ canvas1.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define ROWS 30
-#define COLS 60
+#define WIDTH 80
+#define HEIGHT 24
 #define MAX_OBJECTS 100
 
-char canvas[ROWS][COLS];
+#define EMPTY '_'
 
 typedef enum {
     LINE = 1,
@@ -17,31 +17,33 @@ typedef enum {
 
 typedef struct {
     ShapeType type;
-    int x1, y1, x2, y2, x3, y3;
+    int x1, y1, x2, y2;
+    int x3, y3;
     int radius;
     int active;
     char ch;
 } Object;
 
+char picture[HEIGHT][WIDTH];
 Object objects[MAX_OBJECTS];
 int objectCount = 0;
 
 /* Canvas functions */
-void initCanvas() {
-    for (int i = 0; i < ROWS; i++)
-        for (int j = 0; j < COLS; j++)
-            canvas[i][j] = ' ';
+void clearPicture() {
+    for (int i = 0; i < HEIGHT; i++)
+        for (int j = 0; j < WIDTH; j++)
+            picture[i][j] = EMPTY;
 }
 
-void plot(int x, int y, char ch) {
-    if (x >= 0 && x < COLS && y >= 0 && y < ROWS)
-        canvas[y][x] = ch;
+void setPixel(int x, int y, char ch) {
+    if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+        picture[y][x] = ch;
 }
 
-void displayCanvas() {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++)
-            printf("%c", canvas[i][j]);
+void displayPicture() {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++)
+            printf("%c", picture[i][j]);
         printf("\n");
     }
 }
@@ -54,7 +56,7 @@ void drawLine(int x1, int y1, int x2, int y2, char ch) {
     int err = dx - dy;
 
     while (1) {
-        plot(x1, y1, ch);
+        setPixel(x1, y1, ch);
         if (x1 == x2 && y1 == y2) break;
         int e2 = 2 * err;
         if (e2 > -dy) { err -= dy; x1 += sx; }
@@ -62,46 +64,61 @@ void drawLine(int x1, int y1, int x2, int y2, char ch) {
     }
 }
 
-void drawRectangle(int x, int y, int w, int h, char ch) {
-    for (int i = x; i < x + w; i++) {
-        plot(i, y, ch);
-        plot(i, y + h - 1, ch);
-    }
-    for (int i = y; i < y + h; i++) {
-        plot(x, i, ch);
-        plot(x + w - 1, i, ch);
+void drawRectangle(int x1, int y1, int x2, int y2, char ch) {
+    for (int y = y1; y <= y2; y++)
+        for (int x = x1; x <= x2; x++)
+            setPixel(x, y, ch);
+}
+
+void drawCircle(int cx, int cy, int radius, char ch) {
+    for (int y = cy - radius; y <= cy + radius; y++) {
+        for (int x = cx - radius; x <= cx + radius; x++) {
+            double dist = sqrt((x - cx) * (x - cx) + (y - cy) * (y - cy));
+            if (dist <= radius) setPixel(x, y, ch);
+        }
     }
 }
 
-void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, char ch) {
-    drawLine(x1, y1, x2, y2, ch);
-    drawLine(x2, y2, x3, y3, ch);
-    drawLine(x3, y3, x1, y1, ch);
+/* Helper for triangle filling */
+int interpolate(int x0, int y0, int x1, int y1, int y) {
+    if (y1 == y0) return x0;
+    return x0 + (x1 - x0) * (y - y0) / (y1 - y0);
 }
 
-void drawCircle(int xc, int yc, int r, char ch) {
-    int x = 0, y = r, d = 3 - 2 * r;
-    while (x <= y) {
-        plot(xc + x, yc + y, ch); plot(xc - x, yc + y, ch);
-        plot(xc + x, yc - y, ch); plot(xc - x, yc - y, ch);
-        plot(xc + y, yc + x, ch); plot(xc - y, yc + x, ch);
-        plot(xc + y, yc - x, ch); plot(xc - y, yc - x, ch);
-        if (d < 0) d += 4 * x + 6;
-        else { d += 4 * (x - y) + 10; y--; }
-        x++;
+/* Filled triangle using scanline */
+void fillTriangle(int x1, int y1, int x2, int y2, int x3, int y3, char ch) {
+    // Sort vertices by y-coordinate
+    if (y2 < y1) { int tx=x1; x1=x2; x2=tx; int ty=y1; y1=y2; y2=ty; }
+    if (y3 < y1) { int tx=x1; x1=x3; x3=tx; int ty=y1; y1=y3; y3=ty; }
+    if (y3 < y2) { int tx=x2; x2=x3; x3=tx; int ty=y2; y2=y3; y3=ty; }
+
+    // Upper part
+    for (int y = y1; y <= y2; y++) {
+        int xa = interpolate(x1,y1,x3,y3,y);
+        int xb = interpolate(x1,y1,x2,y2,y);
+        if (xa > xb) { int tmp=xa; xa=xb; xb=tmp; }
+        for (int x = xa; x <= xb; x++) setPixel(x,y,ch);
+    }
+
+    // Lower part
+    for (int y = y2; y <= y3; y++) {
+        int xa = interpolate(x1,y1,x3,y3,y);
+        int xb = interpolate(x2,y2,x3,y3,y);
+        if (xa > xb) { int tmp=xa; xa=xb; xb=tmp; }
+        for (int x = xa; x <= xb; x++) setPixel(x,y,ch);
     }
 }
 
 /* Render all active objects */
 void renderObjects() {
-    initCanvas();
+    clearPicture();
     for (int i = 0; i < objectCount; i++) {
         if (!objects[i].active) continue;
         switch (objects[i].type) {
             case LINE: drawLine(objects[i].x1, objects[i].y1, objects[i].x2, objects[i].y2, objects[i].ch); break;
             case RECTANGLE: drawRectangle(objects[i].x1, objects[i].y1, objects[i].x2, objects[i].y2, objects[i].ch); break;
             case CIRCLE: drawCircle(objects[i].x1, objects[i].y1, objects[i].radius, objects[i].ch); break;
-            case TRIANGLE: drawTriangle(objects[i].x1, objects[i].y1, objects[i].x2, objects[i].y2, objects[i].x3, objects[i].y3, objects[i].ch); break;
+            case TRIANGLE: fillTriangle(objects[i].x1, objects[i].y1, objects[i].x2, objects[i].y2, objects[i].x3, objects[i].y3, objects[i].ch); break;
         }
     }
 }
@@ -109,53 +126,51 @@ void renderObjects() {
 /* Object management */
 void addObject() {
     Object obj; int shape;
-    printf("Choose shape (1=Line,2=Rectangle,3=Circle,4=Triangle): ");
+    printf("\nChoose shape type:\n1. Line\n2. Rectangle\n3. Circle\n4. Triangle\nEnter shape type: ");
     scanf("%d", &shape);
     obj.type = shape; obj.active = 1;
-    printf("Enter character to draw with: "); scanf(" %c", &obj.ch);
 
-    if (shape == LINE) scanf("%d %d %d %d", &obj.x1,&obj.y1,&obj.x2,&obj.y2);
-    else if (shape == RECTANGLE) scanf("%d %d %d %d", &obj.x1,&obj.y1,&obj.x2,&obj.y2);
-    else if (shape == CIRCLE) scanf("%d %d %d", &obj.x1,&obj.y1,&obj.radius);
-    else if (shape == TRIANGLE) scanf("%d %d %d %d %d %d", &obj.x1,&obj.y1,&obj.x2,&obj.y2,&obj.x3,&obj.y3);
+    printf("\nEnter character to draw with: ");
+    scanf(" %c", &obj.ch);
 
-    objects[objectCount++] = obj;
-}
+    if (shape == LINE) {
+        printf("Enter x1 y1 x2 y2: ");
+        scanf("%d %d %d %d", &obj.x1,&obj.y1,&obj.x2,&obj.y2);
+    } else if (shape == RECTANGLE) {
+        printf("Enter top-left x y and bottom-right x y: ");
+        scanf("%d %d %d %d", &obj.x1,&obj.y1,&obj.x2,&obj.y2);
+    } else if (shape == CIRCLE) {
+        printf("Enter center x y and radius: ");
+        scanf("%d %d %d", &obj.x1,&obj.y1,&obj.radius);
+    } else if (shape == TRIANGLE) {
+        printf("Enter x1 y1 x2 y2 x3 y3: ");
+        scanf("%d %d %d %d %d %d", &obj.x1,&obj.y1,&obj.x2,&obj.y2,&obj.x3,&obj.y3);
+    }
 
-void deleteObject() {
-    int idx; printf("Enter object index to delete: "); scanf("%d", &idx);
-    if (idx >= 0 && idx < objectCount) objects[idx].active = 0;
-}
-
-void modifyObject() {
-    int idx; printf("Enter object index to modify: "); scanf("%d", &idx);
-    if (idx < 0 || idx >= objectCount || !objects[idx].active) return;
-    Object *obj = &objects[idx];
-    printf("Enter new character: "); scanf(" %c", &obj->ch);
-    if (obj->type == LINE) scanf("%d %d %d %d", &obj->x1,&obj->y1,&obj->x2,&obj->y2);
-    else if (obj->type == RECTANGLE) scanf("%d %d %d %d", &obj->x1,&obj->y1,&obj->x2,&obj->y2);
-    else if (obj->type == CIRCLE) scanf("%d %d %d", &obj->x1,&obj->y1,&obj->radius);
-    else if (obj->type == TRIANGLE) scanf("%d %d %d %d %d %d", &obj->x1,&obj->y1,&obj->x2,&obj->y2,&obj->x3,&obj->y3);
+    objects[objectCount] = obj;
+    objectCount++;
 }
 
 void listObjects() {
     for (int i = 0; i < objectCount; i++)
-        if (objects[i].active) printf("Index %d Type %d Char %c\n", i, objects[i].type, objects[i].ch);
+        if (objects[i].active)
+            printf("Index %d Type %d Char %c\n", i, objects[i].type, objects[i].ch);
 }
 
 /* Main menu */
 int main() {
     int choice;
     while (1) {
-        printf("\n--- ASCII Graphics Editor ---\n");
-        printf("1. Add object\n2. Delete object\n3. Modify object\n4. Display picture\n5. List objects\n0. Exit\nChoice: ");
+        printf("2D Graphics Editor\n");
+        printf("1. Add object\n2. Delete object\n3. Modify object\n4. Display picture\n5. List objects\n0. Exit\nEnter choice: ");
         scanf("%d", &choice);
+
         if (choice == 1) addObject();
-        else if (choice == 2) deleteObject();
-        else if (choice == 3) modifyObject();
-        else if (choice == 4) { renderObjects(); displayCanvas(); }
+        else if (choice == 4) { renderObjects(); printf("\n"); displayPicture(); }
         else if (choice == 5) listObjects();
         else if (choice == 0) break;
+
+        printf("\n");
     }
     return 0;
 }
